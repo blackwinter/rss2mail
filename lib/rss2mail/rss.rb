8aa5bfa916a190d108325804dbc34ee717750954
Jens Wille <jens.wille@uni-koeln.de>
2011-04-29 15:06:07 +0200

    Switched license to Affero GPL.

--- lib/rss2mail/rss.rb
+++ lib/rss2mail/rss.rb
@@ -5,23 +5,23 @@
 #                                                                             #
 # A component of rss2mail, the RSS to e-mail forwarder.                       #
 #                                                                             #
-# Copyright (C) 2007-2009 Jens Wille                                          #
+# Copyright (C) 2007-2011 Jens Wille                                          #
 #                                                                             #
 # Authors:                                                                    #
 #     Jens Wille <ww@blackwinter.de>                                          #
 #                                                                             #
 # rss2mail is free software; you can redistribute it and/or modify it under   #
-# the terms of the GNU General Public License as published by the Free        #
+# the terms of the GNU Affero General Public License as published by the Free #
 # Software Foundation; either version 3 of the License, or (at your option)   #
 # any later version.                                                          #
 #                                                                             #
 # rss2mail is distributed in the hope that it will be useful, but WITHOUT ANY #
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   #
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       #
-# details.                                                                    #
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for     #
+# more details.                                                               #
 #                                                                             #
-# You should have received a copy of the GNU General Public License along     #
-# with rss2mail. If not, see <http://www.gnu.org/licenses/>.                  #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with rss2mail. If not, see <http://www.gnu.org/licenses/>.            #
 #                                                                             #
 ###############################################################################
 #++
